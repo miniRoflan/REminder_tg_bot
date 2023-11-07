25a70fabfbd9dccc1709c67416e5c1305e6618ac
miniRoflan <skamer1233@yandex.ru>
2023-11-07 11:28:29 +0500

    check 2

--- src/main/java/com/botProject/REminder_tg_bot/config/CommentText.java
+++ src/main/java/com/botProject/REminder_tg_bot/config/CommentText.java
@@ -3,7 +3,7 @@ package com.botProject.REminder_tg_bot.config;
 import jakarta.xml.bind.SchemaOutputResolver;
 
 public class CommentText {
-    public static final String HELP_COM = "";
+    public static final String HELP_COM = "hello";
     public static final String START_COM = "";
     public static final String ADD_REMIND_COM = "";
 
