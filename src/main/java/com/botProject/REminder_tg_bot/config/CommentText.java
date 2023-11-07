f29200b007705bcdd51c9fa8951922c4c2bb8d34
miniRoflan <skamer1233@yandex.ru>
2023-11-07 11:24:27 +0500

    check git

---
+++ src/main/java/com/botProject/REminder_tg_bot/config/CommentText.java
@@ -0,0 +1,10 @@
+package com.botProject.REminder_tg_bot.config;
+
+import jakarta.xml.bind.SchemaOutputResolver;
+
+public class CommentText {
+    public static final String HELP_COM = "";
+    public static final String START_COM = "";
+    public static final String ADD_REMIND_COM = "";
+
+}
